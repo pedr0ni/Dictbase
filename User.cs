b730c0bb16ec1d989b24e1740cf5f01112bf8ff5
Matheus Pedroni <pedroni.dev@gmail.com>
2018-08-17 02:29:54 -0300

    first commit

---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp1
+{
+    class User : RelationalModel
+    {
+
+        public User(MySqlConnection con) : base(con)
+        {
+            this.timestamps = false;
+        }
+
+    }
+}
